d181b8d856ad619218f3f36a1aa323607700c776
John Dowd <irishdowd10@gmail.com>
2017-05-26 08:58:15 -0700

    add object class contact w/ constructor and methods for get and set for each property

---
+++ Objects/Contact.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+
+namespace AddressBook.Objects
+{
+  public class Contact
+  {
+    private int      _id;
+    private string   _name;
+    private int      _phone;
+    private string   _address;
+    private static List<Contact> _instances = new List<Contact>{};
+
+    public Book(string name, string phone, string address)
+    {
+      _id       = _instances.Count;
+      _name     = name;
+      _phone   = phone;
+      _address = address;
+
+      _instances.Add(this);
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string newName)
+    {
+      _name = newName;
+    }
+    public string GetPhone()
+    {
+      return _phone;
+    }
+    public void SetPhone(string newPhone)
+    {
+      _phone = newPhone;
+    }
+    public string GetAddress()
+    {
+      return _address;
+    }
+    public void SetAddress(string newAddress)
+    {
+      _address = newAddress;
+    }
+    public static List<Contact> GetAll()
+    {
+      return _instances;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
+    }
+    public static Contact Find(int searchId)
+    {
+      return _instances[searchId];
+    }
+  }
+}
