d90ed4ad01a8f37a4b7a73f605bc43bf989227d1
John Dowd <irishdowd10@gmail.com>
2017-05-26 09:05:54 -0700

    add HomeModule file with initial head info using, namespace and public class

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using AddressBook.Objects;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      
+    }
+  }
+}
