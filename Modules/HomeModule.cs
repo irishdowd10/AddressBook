7fbf32fe7b0095edfe1084a467a53d24d7e918bf
John Dowd <irishdowd10@gmail.com>
2017-05-26 09:58:32 -0700

    update HomeModule, contact_details and contact_form

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -8,7 +8,26 @@ namespace AddressBook
   {
     public HomeModule()
     {
-      
+      Get["/"] = _ => {
+        return View["index.cshtml", Contact.GetAll()];
+      };
+      Get["/contacts/new"] = _ => {
+        return View["Contact_form.cshtml"];
+      };
+      Post["/"] = _ => {
+        Contact newContact = new Contact(Request.Form["new-name"],
+                                   Request.Form["new-phone"],
+                                   Request.Form["new-address"]);
+        return View["index.cshtml", Contact.GetAll()];
+      };
+      Post["/contacts/clear"] = _ => {
+        Contact.ClearAll();
+        return View["clear.cshtml"];
+      };
+      Get["/contacts/{id}"] = parameters => {
+        Contact targetContact = Contact.Find(parameters.id);
+        return View["Contact_details.cshtml", targetContact];
+      };
     }
   }
 }
